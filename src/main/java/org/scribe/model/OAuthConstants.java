81092c1820edf064abb3af6eaf22cf895d5abe0c
George Cao <matrix3456@163.com>
2012-03-30 11:13:36 +0800

    add more OAuth 2.0 constants

--- src/main/java/org/scribe/model/OAuthConstants.java
+++ src/main/java/org/scribe/model/OAuthConstants.java
@@ -45,5 +45,14 @@ public class OAuthConstants
   public static final String CLIENT_SECRET = "client_secret";
   public static final String REDIRECT_URI = "redirect_uri";
   public static final String CODE = "code";
+  public static final String AUTHORIZATION_CODE = "authorization_code";
+  public static final String RESPONSE_TYPE = "response_type";
+  public static final String GRANT_TYPE = "grant_type";
+  public static final String REFRESH_TOKEN = "refresh_token";
+  public static final String USERNAME = "username";
+  public static final String PASSWORD = "password";
+  public static final String EXPIRES_IN = "expires_in";
+  public static final String TOKEN_TYPE = "token_type";
+  public static final String STATE = "state";
   
 }
